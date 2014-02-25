eab639f22ff971bf6433a34575c9d7e074adc298
Andre Kelpe <efeshundertelf@googlemail.com>
2014-02-25 12:20:26 +0100

    move to new apache hadoop 2.3.0

--- modules/hadoop/manifests/init.pp
+++ modules/hadoop/manifests/init.pp
@@ -1,6 +1,6 @@
 class hadoop($slaves_file = undef, $hdfs_site_file = undef) {
 
-  $hadoop_version = "2.2.0"
+  $hadoop_version = "2.3.0"
   $hadoop_home = "/opt/hadoop-${hadoop_version}"
   $hadoop_tarball = "hadoop-${hadoop_version}.tar.gz"
   $hadoop_tarball_checksums = "${hadoop_tarball}.mds"
